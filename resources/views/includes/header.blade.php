99f4b0f33cc1d815d7fa3cb3e496b9b1eea95a10
Jonas Ferreira <suporte@yesflex.com.br>
2022-02-19 15:31:42 -0300

    first commit

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,7 @@
   <section>
       <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
           <a class="navbar-brand" href="{{ route('admin.home') }}">Olá, <?= Auth::user()->name ?></a>
+          <a href="{{ route('admin.search') }}" class="btn btn-outline-success"> Lista de Veículos</a>
           <a class="navbar-brand" href="{{ route('login.logout') }}">Sair <strong>x</strong></a>
       </nav>
   </section>
